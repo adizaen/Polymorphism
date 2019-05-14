0e2d7eda8292744e0fcff943f0b7da4d39a89778
Adi <adizaenul2000@gmail.com>
2019-05-14 14:33:53 +0700

    LatihanPolymorphism

---
+++ LatihanPolymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            //cat.Speak();\
+            animal = cat;
+            animal.Speak();
+
+            Chicken chicken = new Chicken();
+            //chicken.Speak();
+            animal = chicken;
+            animal.Speak();
+
+            Dog dog = new Dog();
+            //dog.Speak();
+            animal = dog;
+            animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
